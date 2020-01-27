dd7f35f3322aa9064beeb60f3f6a6635f96acc55
jiteman <jiteman@gmail.com>
2020-01-27 16:20:34 +0300

    Separating test libraries and launchers... 0

---
+++ sources/ctf_test_lib/jmsd/ctf/tests/ctf_test_main.cpp
@@ -0,0 +1,18 @@
+#include "ctf_test_main.h"
+
+#include "jmsd/ctf/ctf_library_main.h"
+
+
+namespace jmsd {
+namespace tpa {
+namespace gtm {
+
+
+int run_all_gtm_tests( int argc, char const *const argv[] ) {
+	return ctf::ctf_main( argc, argv, false );
+}
+
+
+} // namespace gtm
+} // namespace tpa
+} // namespace jmsd
--- sources/ctf_test_lib/jmsd/tpa/gtm/gtm_test.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "gtm_test_shared_library_support.h"
